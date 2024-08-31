b68f94c37427a298cba68e0caa05b60e66da07cd
Aayush Poudel <110235375+aayushhh44@users.noreply.github.com>
2024-08-31 14:48:53 +0545

    first commit

---
+++ client/vite-project/src/components/Welcome.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {} from 'react-icons/ai'
+import {} from 'react-icons/si'
+import {} from 'react-icons/bs'
+import Loader from './Loader'
+
+
+const Welcome = () => {
+  return (
+    <div className='flex w-full justify-center items-center'>
+      <div className='flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
+
+      </div>
+        
+        
+      
+    </div>
+  )
+}
+
+export default Welcome
